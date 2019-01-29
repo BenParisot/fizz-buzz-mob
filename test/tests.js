4e5821645ba381faab637f4ff7c10193123d80f1
Ben Parisot <me@benparisot.com>
2019-01-29 11:42:32 -0800

    get the test to work

--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,51 @@
 const test = QUnit.test;
 
-test('time to write a test', function(assert) {
-    assert.equal(true, false);
+function fizzBuzz(integer) {
+    let x = '';
+    if(integer % 3 === 0 && integer % 5 === 0) {
+        x = 'fizzbuzz';
+
+    }
+    else if(integer % 3 === 0) {
+        x = 'fizz';
+    }
+    else if(integer % 5 === 0) {
+        x = 'buzz';
+    }
+    else {
+        x = integer;
+    }   
+
+    return x;
+}
+
+test('return fizz if divisible by 3', function(assert) {
+    const integer = 3;
+    const expect = 'fizz';
+    const result = fizzBuzz(integer);
+
+    assert.equal(result, expect);
+});
+
+test('return buzz if divisible by 5', function(assert) {
+    const integer = 5;
+    const expect = 'buzz';
+    const result = fizzBuzz(integer);
+
+    assert.equal(result, expect);
+});
+
+test('return fizzbuzz if divisible by 5 and 3', function(assert) {
+    const integer = 15;
+    const expect = 'fizzbuzz';
+    const result = fizzBuzz(integer);
+
+    assert.equal(result, expect);
+});
+test('return integer if not divisible by 3 or 5', function(assert) {
+    const integer = 7;
+    const expect = integer;
+    const result = fizzBuzz(integer);
+
+    assert.equal(result, expect);
 });
